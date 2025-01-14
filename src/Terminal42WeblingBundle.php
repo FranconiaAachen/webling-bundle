8bff49e5a79268882d30bdd2cb226a5770917376
Thomas Kaltbeitzel <thomas@kaltbeitzel.me>
2025-01-14 22:06:39 +0100

    Changed configuration to AbstractBundle

--- src/Terminal42WeblingBundle.php
+++ src/Terminal42WeblingBundle.php
@@ -4,12 +4,40 @@ declare(strict_types=1);
 
 namespace Terminal42\WeblingBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class Terminal42WeblingBundle extends Bundle
+class Terminal42WeblingBundle extends AbstractBundle
 {
-    public function getPath(): string
+    public function configure(DefinitionConfigurator $definition): void
     {
-        return \dirname(__DIR__);
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('subdomain')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->defaultValue('%env(WEBLING_SUBDOMAIN)%')
+            ->end()
+            ->scalarNode('api_key')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->defaultValue('%env(WEBLING_API_KEY)%')
+            ->end()
+            ->scalarNode('entity_factory')
+            ->cannotBeEmpty()
+            ->defaultValue('Terminal42\WeblingApi\EntityFactory')
+            ->end()
+            ->end();
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+
+        $builder->setParameter('terminal42_webling.subdomain', $config['subdomain']);
+        $builder->setParameter('terminal42_webling.api_key', $config['api_key']);
+        $builder->setParameter('terminal42_webling.entity_factory', $config['entity_factory']);
     }
 }
